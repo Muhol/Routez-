fb23c9a6e389ef64bf32a2d7d70f85ba7913ffb0
Muhol <ianmuhol48@gmail.com>
2026-08-08 17:25:45 +0300

    feat: add iOS Google Maps API key initialization and location permissions to Info.plist & AppDelegate

--- routez/ios/Runner/AppDelegate.swift
+++ routez/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -7,6 +8,8 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey(ProcessInfo.processInfo.environment["MAPS_API_KEY"] ?? "YOUR_API_KEY_HERE")
+    GMSServices.addInternalUsageAttributionID("gmp_git_agentskills_v1")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
